0cb9c6bc506315fddd04a1224e53ba089e62f586
PHYMatheus <matheusguilherme00221@gmail.com>
2026-06-08 13:22:23 -0300

    Estrutura inicial Java + Spring #1

---
+++ src/main/java/br/com/usebela/UsebelaApplication.java
@@ -0,0 +1,13 @@
+package br.com.usebela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsebelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsebelaApplication.class, args);
+	}
+
+}
